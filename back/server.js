23baeecaa8e605c91d3e4c0ef8f4f87331221c91
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-05-12 16:05:02 +0200

    static server

--- back/server.js
+++ back/server.js
@@ -1,1 +1,21 @@
-const express = require('express');
+const express = require("express");
+const serveIndex = require("serve-index");
+
+const app = express();
+const port = 3000;
+
+app.use((req, res, next) => {
+  console.log("req.url", req.url);
+  next();
+});
+
+app.get("/now", (req, res) => {
+  res.json({ date: new Date() });
+});
+
+app.use(express.static("www"));
+app.use(serveIndex("www"));
+
+app.listen(port, () =>
+  console.log(`Example app listening at http://localhost:${port}`)
+);
