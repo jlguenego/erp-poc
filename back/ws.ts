db76751ab1f6615c2f2e115a41fd05f68d13dd97
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-05-14 15:19:39 +0200

    promise test

--- back/ws.ts
+++ back/ws.ts
@@ -1,11 +1,12 @@
 import express from "express";
 import { exposeResource } from "./rest";
+import "./rest-mongo";
 const app = express.Router();
 
 app.use(express.json());
 
 app.get("/date", (req, res) => res.json({ date: new Date() }));
 
-["salarie", "facture", "engin"].forEach(name => exposeResource(app, name));
+["salarie", "facture", "engin"].forEach((name) => exposeResource(app, name));
 
 export const ws = app;
