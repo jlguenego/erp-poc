39da63d94472629316a3507db9a8c296b51f7a5d
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-06-24 16:59:22 +0200

    added dom chantiers

--- front/src/app/projects/projects-routing.module.ts
+++ front/src/app/projects/projects-routing.module.ts
@@ -4,8 +4,8 @@ import { ListComponent } from './list/list.component';
 import { CreateComponent } from './create/create.component';
 
 const routes: Routes = [
-  { path: 'projects', component: ListComponent },
-  { path: 'projects/new', component: CreateComponent },
+  { path: 'chantiers', component: ListComponent },
+  { path: 'chantiers/nouveau', component: CreateComponent },
 ];
 
 @NgModule({
