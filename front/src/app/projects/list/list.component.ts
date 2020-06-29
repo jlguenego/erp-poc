687b11a0e3e6564dd47f6076b2a98965b9a6909c
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-06-29 15:08:37 +0200

    selected + btn suppress

--- front/src/app/projects/list/list.component.ts
+++ front/src/app/projects/list/list.component.ts
@@ -1,16 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 import { ProjectService } from 'src/app/services/project.service';
+import { Project } from 'src/app/interfaces/project';
 
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
-  styleUrls: ['./list.component.scss']
+  styleUrls: ['./list.component.scss'],
 })
 export class ListComponent implements OnInit {
+  selectedProjects: Project[] = [];
 
-  constructor(public projectService: ProjectService) { }
+  constructor(public projectService: ProjectService) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  toggle(p: Project): void {
+    console.log('toggle', p);
+    const index = this.selectedProjects.findIndex((pr) => pr === p);
+    if (index === -1) {
+      this.selectedProjects.push(p);
+      return;
+    }
+    this.selectedProjects.splice(index, 1);
   }
 
+  remove() {
+    console.log('remove');
+  }
 }
