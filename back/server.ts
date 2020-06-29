76a35ff39c60b42ed9a4dd4e2d410bc9da22fa8a
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-06-29 14:04:53 +0200

    remove mongo and postgre from back

--- back/server.ts
+++ back/server.ts
@@ -1,35 +1,36 @@
 import express from "express";
 import serveIndex from "serve-index";
-import mongoose from "mongoose";
+// import mongoose from "mongoose";
 import { ws } from "./ws";
-import { Sequelize } from "sequelize";
+// import { Sequelize } from "sequelize";
 
 async function main() {
   try {
-    console.log("about to connect to mongo...");
-    await mongoose.connect("mongodb://localhost:27017/erp-poc", {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      serverSelectionTimeoutMS: 5000,
-    });
-    console.log("connected to mongo.");
+    // console.log("about to connect to mongo...");
+    // await mongoose.connect("mongodb://localhost:27017/erp-poc", {
+    //   useNewUrlParser: true,
+    //   useUnifiedTopology: true,
+    //   serverSelectionTimeoutMS: 5000,
+    // });
+    // console.log("connected to mongo.");
 
-    console.log("about to connect to postgres...");
-    const sequelize = new Sequelize("mydb", "postgres", "admin", {
-      host: "localhost",
-      port: 5432,
-      dialect: "postgres",
-    });
-    await sequelize.authenticate();
-    console.log("connected to postgres.");
+    // console.log("about to connect to postgres...");
+    // const sequelize = new Sequelize("mydb", "postgres", "admin", {
+    //   host: "localhost",
+    //   port: 5432,
+    //   dialect: "postgres",
+    // });
+    // await sequelize.authenticate();
+    // console.log("connected to postgres.");
+    const sequelize = undefined;
 
     const app = express();
     const port = 3000;
 
     app.use("/ws", await ws(sequelize));
-    console.log("about to sync sequelize...");
-    await sequelize.sync({ force: false });
-    console.log("sync done...");
+    // console.log("about to sync sequelize...");
+    // await sequelize.sync({ force: false });
+    // console.log("sync done...");
 
     app.use(express.static("www"));
     app.use(serveIndex("www"));
--- back/ws.ts
+++ back/ws.ts
@@ -1,26 +1,26 @@
 import express from "express";
 import { exposeResource } from "./rest";
 import "./rest-mongo";
-import { exposeMongoResource } from "./rest-mongo";
-import { Salarie } from "./model/Salarie";
+// import { exposeMongoResource } from "./rest-mongo";
+// import { Salarie } from "./model/Salarie";
 import { Sequelize } from "sequelize/types";
-import { chantier } from "./model-sequelize/Chantier";
-import { exposeSequelizeResource } from "./rest-sequelize";
+// import { chantier } from "./model-sequelize/Chantier";
+// import { exposeSequelizeResource } from "./rest-sequelize";
+import cors from "cors";
 
 export const ws = async function (sequelize: Sequelize) {
   const app = express.Router();
 
   app.use(express.json());
+  app.use(cors());
 
   app.get("/date", (req, res) => res.json({ date: new Date() }));
 
-  ["facture", "engin"].forEach((name) =>
-    exposeResource(app, name)
-  );
+  ["facture", "engin", "chantier"].forEach((name) => exposeResource(app, name));
 
-  exposeMongoResource(app, Salarie);
+  // exposeMongoResource(app, Salarie);
 
-  exposeSequelizeResource(sequelize, app, chantier);
+  // exposeSequelizeResource(sequelize, app, chantier);
 
   return app;
 };
