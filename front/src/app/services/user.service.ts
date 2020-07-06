69446f78e4a7c2c5575cbdbed2efe25e69e3bdb8
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-07-06 12:43:33 +0200

    added js-sha1 to the project

--- front/src/app/services/user.service.ts
+++ front/src/app/services/user.service.ts
@@ -2,19 +2,29 @@ import { Injectable } from '@angular/core';
 import { Observable, Subject } from 'rxjs';
 import { User } from '../interfaces/user';
 import { HttpClient } from '@angular/common/http';
+import sha1 from 'js-sha1';
+
+
+console.log('sha1', sha1('1234'));
 
 @Injectable({
   providedIn: 'root',
 })
 export class UserService {
-  constructor(private http: HttpClient) {}
+  salt = '';
+  constructor(private http: HttpClient) {
+    this.http.get<{ salt: string }>('/ws/salt').subscribe({
+      next: (data) => (this.salt = data.salt),
+      error: (err) => console.error('no salt found'),
+    });
+  }
 
   login(login: string, password: string): Observable<User> {
     const subject = new Subject<User>();
     this.http
       .post<User>('/ws/login', {
         login,
-        password,
+        password: sha1(this.salt + login + this.salt + password + this.salt),
       })
       .subscribe({
         next: (user) => {
